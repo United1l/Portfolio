31e8a2fc8a364b769c431c97fdc03c3c8a661d72
Dark Saeint <michaelasare719#gmail.com>
2022-05-18 16:52:24 +0000

    modified Js
    configuring the mouse gradient

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+// Getting and storing the elements from the page
+
 const switchElement = document.querySelector('.switch');
 const flickerElement = document.querySelector('.flicker');
 const nav = document.querySelector('.nav');
-let Links = nav.querySelectorAll('p');
+const Links = nav.querySelectorAll('p');
 const tagLine = document.querySelector('#tag-line');
 const mainText = document.querySelector('.content-1');
 
 
-const mouseGradient = document.querySelector('.mouse-gradient');
+const mouseGradient = window.getComputedStyle(document.querySelector('.mouse-gradient'));
 
 
+//configuration for the mouse gradient
 
 function randomRGB() {
     return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)},0.4)`;
@@ -28,6 +31,21 @@ function getPositon(elem) {
     return {x: positionX, y: positionY};
 }
 
+document.body.addEventListener('pointermove', (e) => { 
+    let parentPos = getPositon(mouseGradient);
+    let xPosition = e.clientX - parentPos.x - (mouseGradient.offsetWidth / 2);
+    let yPosition = e.clientY - parentPos.y - (mouseGradient.offsetHeight / 2);
+  
+    let translate3dValue = "translate3d(" + xPosition + "px," + yPosition + "px, 0)";
+    mouseGradient.style.transform = translate3dValue;
+    mouseGradient.style.zIndex = -1000;
+
+    let randomColor = randomRGB();
+    mouseGradient.setAttribute('background-image', 'radial-gradient(" + randomColor + "," + randomColor + "," + randomColor + ")')
+  }, false)
+  
+
+//configuration for darkmode
 
 switchElement.addEventListener('click', () => {
     document.body.classList.toggle('dark');
@@ -50,21 +68,3 @@ switchElement.addEventListener('click', () => {
     }
 })
 
-document.body.addEventListener('pointermove', (e) => { 
-  let xPosition = e.clientX - (mouseGradient.offsetWidth /2);
-  let yPosition = e.clientY - (mouseGradient.offsetHeight /2);
-
-  let translate3dValue = "translate3d(" + xPosition + "px," + yPosition + "px, 0)";
-  mouseGradient.style.transform = translate3dValue;
-  mouseGradient.style.zIndex = -1000;
-})
-
-for(let i = 0; i < Links.length; i++) {
-    if(Links[i].onmouseover = true) {
-        Links[i].style.transform = "translateY(-20px)";
-        Links[i].style.transition = "0.3s"
-    } else {
-        Links[i].style.transform = "";
-    }
-    
-}
